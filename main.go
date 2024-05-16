3afb785f2ee429b353f967613ba6bf562743662c
cmdctl <anton.bozhinov@gmail.com>
2024-05-17 01:37:18 +0300

    add reading workitems of teammates

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+
+	"github.com/urfave/cli/v2"
 )
 
 type AzureWorkItems []struct {
@@ -26,6 +29,39 @@ type Fields struct {
 	SystemTitle string `json:"System.Title"`
 }
 
+func ListWorkItems(name string) {
+
+	var workItems AzureWorkItems
+	const query = `az boards query --wiql "SELECT [System.Id], [System.Title], [System.State] FROM WorkItems WHERE [System.AssignedTo] = '%s' AND [System.State] <> 'Done' AND [System.State] <> 'Resolved' AND [System.State] <> 'Closed' AND [System.State] <> 'Removed'"`
+	cmd := exec.Command("bash", "-c", fmt.Sprintf(query, name))
+
+	output, err := cmd.Output()
+	if err != nil {
+		log.Println("could not get command output")
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(output, &workItems)
+	if err != nil {
+		log.Println("could not unmarshal json")
+		log.Fatal(err)
+	}
+	var longestTitle int
+	for _, workItem := range workItems {
+		if len(workItem.Fields.SystemTitle) > longestTitle {
+			longestTitle = len(workItem.Fields.SystemTitle)
+		}
+	}
+
+	for _, workItem := range workItems {
+		padding := longestTitle - len(workItem.Fields.SystemTitle)
+		fmt.Printf("%d | %s", workItem.Fields.SystemID, workItem.Fields.SystemTitle)
+		for i := 0; i < padding; i++ {
+			fmt.Print(" ")
+		}
+		fmt.Printf(" | %s\n", workItem.Fields.SystemState)
+	}
+}
+
 func ListMyWorkItems() {
 	var workItems AzureWorkItems
 	const query = `az boards query --wiql "SELECT [System.Id], [System.Title], [System.State] FROM WorkItems WHERE [System.AssignedTo] = @Me AND [System.State] <> 'Done' AND [System.State] <> 'Resolved' AND [System.State] <> 'Closed' AND [System.State] <> 'Removed'"`
@@ -43,7 +79,8 @@ func ListMyWorkItems() {
 	}
 	var longestTitle int
 	for _, workItem := range workItems {
-		if len(workItem.Fields.SystemTitle) > longestTitle { longestTitle = len(workItem.Fields.SystemTitle)
+		if len(workItem.Fields.SystemTitle) > longestTitle {
+			longestTitle = len(workItem.Fields.SystemTitle)
 		}
 	}
 
@@ -55,9 +92,33 @@ func ListMyWorkItems() {
 		}
 		fmt.Printf(" | %s\n", workItem.Fields.SystemState)
 	}
-
 }
 
 func main() {
-  ListMyWorkItems()
+
+	app := &cli.App{
+		Name:  "mywork",
+		Usage: "List the work items assigned to me",
+		Action: func(*cli.Context) error {
+			ListMyWorkItems()
+			return nil
+		},
+		Commands: []*cli.Command{
+			{
+				Name:    "of",
+				Usage:   "List the work items assigned to a user",
+				Action: func(c *cli.Context) error {
+					name := c.Args().First()
+					ListWorkItems(name)
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
+
+
