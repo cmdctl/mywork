de1cda7f5d7d72cca99e63d257ac110aae25be25
cmdctl <anton.bozhinov@gmail.com>
2024-05-17 01:18:01 +0300

    init

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+type AzureWorkItems []struct {
+	CommentVersionRef CommentVersionRef `json:"commentVersionRef,omitempty"`
+	Fields            Fields            `json:"fields"`
+	ID                int               `json:"id"`
+	Relations         any               `json:"relations"`
+	Rev               int               `json:"rev"`
+	URL               string            `json:"url"`
+}
+type CommentVersionRef struct {
+	CommentID int    `json:"commentId"`
+	URL       string `json:"url"`
+	Version   int    `json:"version"`
+}
+type Fields struct {
+	SystemID    int    `json:"System.Id"`
+	SystemState string `json:"System.State"`
+	SystemTitle string `json:"System.Title"`
+}
+
+func ListMyWorkItems() {
+	var workItems AzureWorkItems
+	const query = `az boards query --wiql "SELECT [System.Id], [System.Title], [System.State] FROM WorkItems WHERE [System.AssignedTo] = @Me AND [System.State] <> 'Done' AND [System.State] <> 'Resolved' AND [System.State] <> 'Closed' AND [System.State] <> 'Removed'"`
+	cmd := exec.Command("bash", "-c", query)
+
+	output, err := cmd.Output()
+	if err != nil {
+		log.Println("could not get command output")
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(output, &workItems)
+	if err != nil {
+		log.Println("could not unmarshal json")
+		log.Fatal(err)
+	}
+	var longestTitle int
+	for _, workItem := range workItems {
+		if len(workItem.Fields.SystemTitle) > longestTitle { longestTitle = len(workItem.Fields.SystemTitle)
+		}
+	}
+
+	for _, workItem := range workItems {
+		padding := longestTitle - len(workItem.Fields.SystemTitle)
+		fmt.Printf("%d | %s", workItem.Fields.SystemID, workItem.Fields.SystemTitle)
+		for i := 0; i < padding; i++ {
+			fmt.Print(" ")
+		}
+		fmt.Printf(" | %s\n", workItem.Fields.SystemState)
+	}
+
+}
+
+func main() {
+  ListMyWorkItems()
+}
